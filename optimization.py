a8dfcf4167bc8dc9402766035065f8711852735b
Maria Sofia Figueiredo <up201904675@fe.up.pt>
2022-04-03 23:34:09 +0100

    Random solution function

--- optimization.py
+++ optimization.py
@@ -1,4 +1,7 @@
-from dataCenterConfig import DataCenterConfig as config
+from random import randint
+from dataCenterConfig import DataCenterConfig, Server
+
+MAX_ALLOC_TRIES = 25
 # Pools example [1,0,0,2,1] 
 # This means servers 2 and 3 are assigned to pool 0,
 # servers 0 and 4 are assigned to pool 1
@@ -15,14 +18,66 @@ dataCenter = [[]]
 # config is an object of the class dataCenterConfig defined in dataCenterCOnfig.py
 # @dataclass
 # class DataCenterConfig:
-#     rows: int 
-#     slots: int 
-#     unavailable: int
-#     pools: int
-#     servers: int
-def randomSolution(config):
+#     rows: int # number of rows
+#     slots: int # number of slots per row
+#     unavailable: array[int] # coordinates of unavailable slots
+#     pools: int # number of pools
+#     servers: array[int] #array with server sizes. len(servers) = number of servers
+
+def randomSolution(config: DataCenterConfig):
     pools = []
-    dataCenter = []
-    
+
+    # Randomly assign pools to servers
+    for i in range(len(config.servers)):
+        pools.append(randint(0,config.pools-1))
+
+    dataCenter =[[-1 for r in range(config.slots)] for s in range(config.rows)]
+
+    # Set unavailable slots accordingly
+    for coords in config.unavailable:
+        print(coords)
+        r,s = coords
+        dataCenter[r][s] = -2
+
+    for index, server in enumerate(config.servers):
+        assigned = False
+        tries = 0
+        while not assigned and tries < MAX_ALLOC_TRIES:
+            tries += 1
+            # Get random row and slot
+            r = randint(0, config.rows-1)
+            s = randint(0, config.slots-1)
+            valid = True
+
+            # Check if server fits at slot s in row r
+            for i in range(server.slots):
+                if(s+i >= config.slots):
+                    valid = False
+                    break
+                if dataCenter[r][s+i] != -1: # -1 means available
+                    valid = False
+
+            # If server doesn't fit it won't be valid
+            if not valid:
+                continue
+            
+            # If it's valid assign it
+            for i in range(server.slots):
+                dataCenter[r][s+i] = index
+
+            assigned = True
+    return (pools, dataCenter)
+
+unavailable = [(0,0)]
+servers = [Server(3,10), Server(3,10), Server(2,5), Server(1,5), Server(1,1)]
+rows = 2
+slots = 5
+pools = 2
+config = DataCenterConfig(rows,slots,unavailable,pools,servers)
+
+pools, dataCenter = randomSolution(config)
+
+print("Pools: ", pools)
+print("DataCenter: ", dataCenter)
 
 
