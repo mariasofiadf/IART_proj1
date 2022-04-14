1bdb242fdbaa840aa11998653a44103d447b0a7c
ritaramada <anaritaramada@gmail.com>
2022-04-14 19:13:40 +0100

    changed evaluation function#

--- simAnnealing.py
+++ simAnnealing.py
@@ -1,5 +1,5 @@
 from dataCenter import DataCenter, Solution, Server
-from evaluation import evaluate
+from evaluation import evaluate_solution
 from neighbourhood import Neighbourhood, neighbourhood
 from solution import randomSolution
 from read import readDataCenter
@@ -9,14 +9,14 @@ from numpy.random import rand
 def simulatedAnnealing(config: DataCenter, iterations: int, neighbourModes, init_temp):
     #initial point
     best = randomSolution(config)
-    best_eval = evaluate(best, config)
+    best_eval = evaluate_solution(best, config)
     curr, curr_eval = best, best_eval
     temp = init_temp
     for i in range (iterations):
         temp -=  init_temp/iterations
         #new solution
         new_sol = neighbourhood(curr,neighbourModes,config)
-        new_sol_eval = evaluate(new_sol, config)
+        new_sol_eval = evaluate_solution(new_sol, config)
 
         diff = new_sol_eval - curr_eval
         probabilty = exp((-diff / temp))
@@ -37,8 +37,8 @@ config = readDataCenter('problem.txt')
 
 neighbourModes = [Neighbourhood.ADD_SV,Neighbourhood.RMV_SV,Neighbourhood.SWTCH_SV_POOL]
 
-temp = 10
-iter = 10000
+temp = 100
+iter = 4500
 
 
 sol = simulatedAnnealing(config, iter, neighbourModes, temp)
