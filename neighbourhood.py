a54692c552c238af68c74ce7612cad4c5b58ba1e
Tomás Morgado Vicente <up201904609@edu.fe.up.pt>
2022-04-14 20:53:33 +0100

    Finish neighbourhood.

--- neighbourhood.py
+++ neighbourhood.py
@@ -1,12 +1,9 @@
-from pydoc import doc
 import random
-from dataCenter import Solution, DataCenter, Server
 from enum import Enum
-from typing import List
-import numpy as np
-from random import randint
 
-from utils import assignServer
+import numpy as np
+
+from dataCenter import Solution, DataCenter, Server
 
 
 class Neighbourhood(Enum):
@@ -17,42 +14,116 @@ class Neighbourhood(Enum):
     SWTCH_SV_POOL = 4
 
 
-def moveServer(solution: Solution, server: Server):
+def neighbourhood(solution: Solution, modes: [Neighbourhood], config: DataCenter):
+    solution_copy = solution.deepcopy()
+    mode = random.choice(modes)
+
+    allocated_server_nos = [x for x in set(np.array(solution_copy.dataCenter).flatten()) if x >= 0]
+    unallocated_servers_nos = [i for i in range(len(config.servers)) if i not in allocated_server_nos]
+
+    if mode == Neighbourhood.ADD_SV and len(unallocated_servers_nos) > 0:
+        server_no = random.choice(unallocated_servers_nos)
+        return assign_server_to_first_available_slot(solution_copy, config.servers[server_no])
+    elif len(allocated_server_nos) > 0:
+        server_no = random.choice(allocated_server_nos)
+        if mode == Neighbourhood.RMV_SV:
+            return unassign_server(solution_copy, config.servers[server_no])
+        elif mode == Neighbourhood.MOV_SV_SLOT:
+            return move_server_inside_row(solution_copy, config.servers[server_no])
+        elif mode == Neighbourhood.SWTCH_SV_ROW:
+            return move_server_to_different_row(solution_copy, config.servers[server_no])
+        elif mode == Neighbourhood.SWTCH_SV_POOL:
+            return move_server_to_different_pool(solution_copy, config.servers[server_no])
+    return None
+
+
+def move_server_inside_row(solution: Solution, server: Server):
+    old_slot: int
+    for row_no, row in enumerate(solution.dataCenter):
+        for slot_no, slot in enumerate(row):
+            if slot == server.id:
+                old_slot = slot_no
+                break  # Stop searching inside row
+        else:
+            continue  # Next row
+        row[old_slot:old_slot + server.slots] = [-1] * server.slots
+        for slot_no, slot in enumerate(row):
+            if slot_no != old_slot and row[slot_no:slot_no + server.slots] == [-1] * server.slots:
+                solution.dataCenter[row_no][slot_no:slot_no + server.slots] = [server.id] * server.slots
+                break  # Stop searching inside row
+        else:
+            return None  # No other slot
     return solution
 
 
-def neighbourhood(solution: Solution, modes: List[Neighbourhood], config: DataCenter):
-    solutionCopy = solution.deepcopy()
-    index = random.randint(0, len(modes) - 1)
-    mode = modes[index]
+def move_server_to_different_row(solution: Solution, server: Server):
+    if len(solution.dataCenter) < 2:
+        return None  # No other row
 
-    servers = [x for x in set(np.array(solutionCopy.dataCenter).flatten()) if x >= 0]
-    nonAllocatedServers = [i for i, x in enumerate(config.servers) if i not in servers]
+    old_row_no: int
+    for row_no, row in enumerate(solution.dataCenter):
+        for slot_no, slot in enumerate(row):
+            if slot == server.id:
+                old_row_no = row_no
+                break  # Stop searching inside row
+        else:
+            continue  # Next row
+        break  # Stop searchhing rows
+    else:
+        return None  # Not found
 
-    if mode == Neighbourhood.RMV_SV and len(servers) > 0:
-        toRmv = random.randint(0, len(servers) - 1)
-        for row in solutionCopy.dataCenter:
-            for i, slot in enumerate(row):
-                if slot == servers[toRmv]:
-                    row[i] = -1
+    unassign_server(solution, server)
+    new_row_no = old_row_no
+    while new_row_no == old_row_no:
+        new_row_no = random.randint(0, len(solution.dataCenter) - 1)
 
-    if mode == Neighbourhood.ADD_SV:
-        toAdd = random.choice(nonAllocatedServers)
-        assignServer(solutionCopy, config, config.servers[toAdd])
+    for slot_no in range(len(solution.dataCenter[new_row_no])):
+        if solution.dataCenter[new_row_no][slot_no:slot_no + server.slots] == [-1] * server.slots:
+            solution.dataCenter[new_row_no][slot_no:slot_no + server.slots] = [server.id] * server.slots
+            break
+    else:
+        return None
 
-    # if(mode == Neighbourhood.MOV_SV_SLOT):
-    #     toMove = random.choice(servers)
-    #     moveServer(solution,config.servers[toMove])
+    return solution
 
-    # if(mode == Neighbourhood.SWTCH_SV_ROW):
-    #     toSwitch = random.choice(servers)
-    #     assignServer(solution, config, config.servers[toSwitch])
 
-    if mode == Neighbourhood.SWTCH_SV_POOL and len(servers) > 0:
-        toSwitch = random.choice(servers)
-        r = solutionCopy.pools[toSwitch]
-        while r == solutionCopy.pools[toSwitch]:
-            r = randint(0, config.pools - 1)
-        solutionCopy.pools[toSwitch] = r
+def move_server_to_different_pool(solution: Solution, server: Server):
+    if len(set(solution.pools)) < 2:
+        return None  # No other pool exists
+    no_pools = len(set(solution.pools))
+    old_pool_no = solution.pools[server.id]
+    new_pool_no = old_pool_no
+    while new_pool_no == old_pool_no:
+        new_pool_no = random.randint(0, len(set(solution.pools)) - 1)
+    solution.pools[server.id] = new_pool_no
+    if len(set(solution.pools)) < no_pools:
+        return None  # One pool left unassigned
+    return solution
 
-    return solutionCopy
+
+def unassign_server(solution: Solution, server: Server):
+    for row_no, row in enumerate(solution.dataCenter):
+        for slot_no, slot in enumerate(row):
+            if slot == server.id:
+                solution.dataCenter[row_no][slot_no:server.slots] = [-1] * server.slots
+                break  # Stop searching inside row
+        else:
+            continue  # Next row
+        break  # Stop searching rows
+    else:
+        return None  # Not found
+    return solution
+
+
+def assign_server_to_first_available_slot(solution: Solution, server: Server):
+    for row_no, row in enumerate(solution.dataCenter):
+        for slot_no in range(len(row)):
+            if row[slot_no:slot_no + server.slots] == [-1] * server.slots:
+                solution.dataCenter[row_no][slot_no:slot_no + server.slots] = [server.id] * server.slots
+                break  # Stop searching inside row
+        else:
+            continue  # Next row
+        break  # Stop searching rows
+    else:
+        return None  # Not found
+    return solution
