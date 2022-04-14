7b15246b57bf006803dfff4b0fda7a7356b5bbf5
Tomás Morgado Vicente <up201904609@edu.fe.up.pt>
2022-04-14 19:46:17 +0100

    Reformat file.

--- neighbourhood.py
+++ neighbourhood.py
@@ -8,6 +8,7 @@ from random import randint
 
 from utils import assignServer
 
+
 class Neighbourhood(Enum):
     RMV_SV = 0
     ADD_SV = 1
@@ -15,45 +16,43 @@ class Neighbourhood(Enum):
     SWTCH_SV_ROW = 3
     SWTCH_SV_POOL = 4
 
+
 def moveServer(solution: Solution, server: Server):
     return solution
 
 
-
 def neighbourhood(solution: Solution, modes: List[Neighbourhood], config: DataCenter):
-    solutionCopy = solution.deepcopy() 
-    index = random.randint(0, len(modes)-1)
+    solutionCopy = solution.deepcopy()
+    index = random.randint(0, len(modes) - 1)
     mode = modes[index]
 
-    servers = [x for x in set(np.array(solutionCopy.dataCenter).flatten()) if x >=0]
-    nonAllocatedServers = [i for i,x in enumerate(config.servers) if i not in servers]
+    servers = [x for x in set(np.array(solutionCopy.dataCenter).flatten()) if x >= 0]
+    nonAllocatedServers = [i for i, x in enumerate(config.servers) if i not in servers]
 
-    if(mode == Neighbourhood.RMV_SV and len(servers)>0):
-        toRmv = random.randint(0, len(servers)-1)
+    if (mode == Neighbourhood.RMV_SV and len(servers) > 0):
+        toRmv = random.randint(0, len(servers) - 1)
         for row in solutionCopy.dataCenter:
             for i, slot in enumerate(row):
-                if(slot == servers[toRmv]):
+                if (slot == servers[toRmv]):
                     row[i] = -1
 
-    if(mode == Neighbourhood.ADD_SV):
+    if (mode == Neighbourhood.ADD_SV):
         toAdd = random.choice(nonAllocatedServers)
         assignServer(solutionCopy, config, config.servers[toAdd])
-    
+
     # if(mode == Neighbourhood.MOV_SV_SLOT):
     #     toMove = random.choice(servers)
     #     moveServer(solution,config.servers[toMove])
 
-    
     # if(mode == Neighbourhood.SWTCH_SV_ROW):
     #     toSwitch = random.choice(servers)
     #     assignServer(solution, config, config.servers[toSwitch])
 
-    
-    if(mode == Neighbourhood.SWTCH_SV_POOL and len(servers)>0):
+    if (mode == Neighbourhood.SWTCH_SV_POOL and len(servers) > 0):
         toSwitch = random.choice(servers)
         r = solutionCopy.pools[toSwitch]
-        while (r==solutionCopy.pools[toSwitch]):
-            r = randint(0, config.pools-1)
+        while (r == solutionCopy.pools[toSwitch]):
+            r = randint(0, config.pools - 1)
         solutionCopy.pools[toSwitch] = r
 
     return solutionCopy
