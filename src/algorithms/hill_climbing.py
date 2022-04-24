ccff38e8ccaa8b95d06a4a3366566fd0fdc2fb02
Tomás Morgado Vicente <up201904609@edu.fe.up.pt>
2022-04-24 01:43:12 +0100

    Add hill climbing (steepest ascent).

--- src/algorithms/hill_climbing.py
+++ src/algorithms/hill_climbing.py
@@ -1,8 +1,20 @@
-from src.neighbourhood.neighbourhood import get_random_neighbour
+from src.neighbourhood.neighbourhood import get_random_neighbour, get_neighbours
 from src.solution.data_center import DataCenter
 from src.solution.evaluation import evaluate_solution
 from src.solution.solution import random_solution
 
+
+def hill_climbing_steepest_ascent(config: DataCenter, iterations: int):
+    solution = random_solution(config)
+    for i in range(iterations):
+        neighbours = get_neighbours(solution, config)
+        newSolution = max(neighbours, key=lambda x: evaluate_solution(x, config))
+        if evaluate_solution(newSolution, config) >= evaluate_solution(solution, config):
+            print('>%d f(%s) => %.5f' % (i, newSolution, evaluate_solution(solution, config)))
+            solution = newSolution
+        else:
+            break
+    return solution
 
 def hill_climbing(config: DataCenter, iterations: int, neighbour_modes):
     solution = random_solution(config)
